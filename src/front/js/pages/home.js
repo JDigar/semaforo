1918fb7aee82db9b360f60d648f647ab52c2affd
JDigar <jdigar96@gmail.com>
2023-07-27 09:08:01 +0000

    light traffic creado

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,31 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const [selectedColor, setSelectedColor] = useState("red");
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="divCenter">
+      <div className="p-4 traffic">
+        <div
+          onClick={() => setSelectedColor("green")}
+          className={
+            "light green" + (selectedColor === "green" ? " glow" : " ")
+          }
+        ></div>
+
+        <div
+          onClick={() => setSelectedColor("yellow")}
+          className={
+            "light yellow" + (selectedColor === "yellow" ? " glow" : " ")
+          }
+        ></div>
+
+        <div
+          onClick={() => setSelectedColor("red")}
+          className={"light red" + (selectedColor === "red" ? " glow" : " ")}
+        ></div>
+      </div>
+    </div>
+  );
 };
